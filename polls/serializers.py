90bcf567fc03ce61ef37b38a5c8e6c166831cf9e
MikhailGlazunov <86202930+MikhailGlazunov@users.noreply.github.com>
2021-06-28 17:40:11 +0400

    Delete serializers.py

--- polls/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Feedback
-
-
-class FeedbackSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = ('author', 'text', 'rating')
