92407c178af3b15dad14dbc18d8f9655d7d62966
MikhailGlazunov <86202930+MikhailGlazunov@users.noreply.github.com>
2021-06-28 17:40:44 +0400

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Rating(models.IntegerChoices):
-    LOW = 1, '1'
-    NORMAL = 2, '2'
-    HIGH = 3, '3'
-    VHIGH = 4, '4'
-    VVHIGH = 5, '5'
+    VERYLOW = 1, '1'
+    LOW = 2, '2'
+    NORMAL = 3, '3'
+    HIGH = 4, '4'
+    VERYHIGH = 5, '5'
 
 
 class Feedback(models.Model):
-    author = models.CharField(max_length=20)
-    text = models.CharField(max_length=120)
-    rating = models.IntegerField(default=Rating.LOW, choices=Rating.choices)
+    author = models.CharField(max_length=20, verbose_name='Автор')
+    text = models.CharField(max_length=120, verbose_name='Текст')
+    rating = models.IntegerField(default=Rating.VERYLOW, choices=Rating.choices, verbose_name='Оценка')
+
+    def get_absolute_url(self):
+        return reverse('create')
