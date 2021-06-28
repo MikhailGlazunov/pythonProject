e8ad7cbb1b08ec43f966423d06fba8809b50025f
MikhailGlazunov <86202930+MikhailGlazunov@users.noreply.github.com>
2021-06-28 17:41:32 +0400

    Update forms.py

--- polls/forms.py
+++ polls/forms.py
@@ -1,12 +1,8 @@
 from django.forms import ModelForm
 from polls.models import Feedback
-from django import forms
 
 
 class FeedbackForm(ModelForm):
-    author = forms.CharField(max_length=20, label="Автор")
-    text = forms.CharField(max_length=120, label="Текст")
-    rating = forms.IntegerField(min_value=1, max_value=5, label="Оценка")
 
     class Meta:
         model = Feedback
